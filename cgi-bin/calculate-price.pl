92276ca204da40593970a197cf00d91cd47fa81b
data-miner00 <data-miner00@users.noreply.github.com>
2024-03-08 20:26:16 +0800

    feat: Add calculate price

---
+++ cgi-bin/calculate-price.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use CGI qw(:standard);
+use CGI::Carp qw(fatalsToBrowser);
+print "Content-Type: text/html\n\n";
+
+#### Read form data
+my $user = param('username');
+my $item = param('item');
+my $qty = param('quantity');
+my $price = param('price');
+
+#### Do the math
+my $subtotal = $qty * $price;
+my $tax = $subtotal * 0.07;
+my $total = $subtotal + $tax;
+
+#### display results
+print "<h2>ECommerce platform</h2>";
+print "<p>Here is your order: </p>";
+print "<p>Item ordered: $item, unit price: \$$price, quantity: $qty </p>";
+print "<p>Subtotal: \$$subtotal </p>";
+print "<p>Sales tax: \$%.2f </p>", $tax;
